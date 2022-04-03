f3db7ff61a1f7c239c09974b9457c4fc194d699f
Alan Poveda <alsepoveda@gmail.com>
2022-04-03 17:29:45 -0300

    Make a test a new pararell function, whit sucess and error

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,7 +1,8 @@
 defmodule ReportsGenerator do
-  alias ReportsGenerator.Parse #Para reduzir o nome
+  # Para reduzir o nome
+  alias ReportsGenerator.Parse
 
-  #variable global para ser acessada somente nesse módulo
+  # variable global para ser acessada somente nesse módulo
   @avaliable_foods [
     "açaí",
     "churrasco",
@@ -21,15 +22,25 @@ defmodule ReportsGenerator do
     |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
   end
 
+  # Tratando erro de papallel
+  def build_parallel(file_name) when not is_list(file_name) do
+    {:error, "Please provide a list of strings"}
+  end
   # Agora Build em paralelo, assim podendo reduzir o tempo de resposta
   def build_parallel(files_names) do
-    files_names
-    |> Task.async_stream(&build/1) #chamando a função de cima, e ainda percorrendo
-    |> Enum.reduce(report_acc(), fn {:ok , result}, report -> sum_reports(report, result) end)
+    result =
+      files_names
+      |> Task.async_stream(&build/1)
+      |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+    {:ok, result}
   end
 
   # Somar os reports que
-  defp sum_reports(%{"foods" => foods1, "users" => users1}, %{"foods" => foods2, "users" => users2}) do
+  defp sum_reports(
+         %{"foods" => foods1, "users" => users1},
+         %{"foods" => foods2, "users" => users2}
+       ) do
     foods = merge_reports(foods1, foods2)
     users = merge_reports(users1, users2)
     %{"foods" => foods, "users" => users}
@@ -42,10 +53,10 @@ defmodule ReportsGenerator do
 
   # Aqui ele já recebe a lista toda e ainda retorna o valor maior
   def higher_value(record, option) when option in @options do
-   {:ok, Enum.max_by(record[option], fn {_key, value} -> value end)}
+    {:ok, Enum.max_by(record[option], fn {_key, value} -> value end)}
   end
 
-  #Tratativa de erro, se não tiver a opção valida
+  # Tratativa de erro, se não tiver a opção valida
   def higher_value(_record, _option), do: {:error, "Invalid option!"}
 
   # Aqui ele soma os valores e ainda retorna um map novo
@@ -53,7 +64,7 @@ defmodule ReportsGenerator do
     users = Map.put(users, id, users[id] + price)
     foods = Map.put(foods, food_name, foods[food_name] + 1)
 
-    %{ report | "users" => users, "foods" => foods}
+    %{report | "users" => users, "foods" => foods}
   end
 
   # Ele criar um map zerado para poder fazer a soma dos itens
--- test/reports_generator_test.exs
+++ test/reports_generator_test.exs
@@ -58,6 +58,78 @@ defmodule ReportsGeneratorTest do
     end
   end
 
+  describe "build_parallel/1" do
+    test "When a file list is provided, Build the report" do
+      files_names = ["report_test.csv", "report_test.csv"]
+
+      response =
+        files_names
+        |> ReportsGenerator.build_parallel()
+
+      expected_response =
+        {:ok,
+         %{
+           "foods" => %{
+             "açaí" => 2,
+             "churrasco" => 4,
+             "esfirra" => 6,
+             "hambúrguer" => 4,
+             "pastel" => 0,
+             "pizza" => 4,
+             "prato_feito" => 0,
+             "sushi" => 0
+           },
+           "users" => %{
+             "1" => 96,
+             "10" => 72,
+             "11" => 0,
+             "12" => 0,
+             "13" => 0,
+             "14" => 0,
+             "15" => 0,
+             "16" => 0,
+             "17" => 0,
+             "18" => 0,
+             "19" => 0,
+             "2" => 90,
+             "20" => 0,
+             "21" => 0,
+             "22" => 0,
+             "23" => 0,
+             "24" => 0,
+             "25" => 0,
+             "26" => 0,
+             "27" => 0,
+             "28" => 0,
+             "29" => 0,
+             "3" => 62,
+             "30" => 0,
+             "4" => 84,
+             "5" => 98,
+             "6" => 36,
+             "7" => 54,
+             "8" => 50,
+             "9" => 48
+           }
+         }}
+
+      assert response == expected_response
+    end
+
+    test "To not a put a list of reports, return a error" do
+      files_name = "error"
+
+      response =
+        files_name
+        |> ReportsGenerator.build_parallel()
+
+      expected_response = {:error, "Please provide a list of strings"}
+
+      assert response == expected_response
+
+    end
+  end
+
   describe "higher_value/2" do
     test "When the option is 'users' return the user most spend" do
       file_name = "report_test.csv"
